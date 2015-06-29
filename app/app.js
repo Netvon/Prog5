ab919b42ea817fcc88e05bd808f2d821bb053b9b
Stijn Smulders <Linksonder@gmail.com>
2015-06-29 14:04:56 +0200

    Material Prog5

--- app/app.js
+++ app/app.js
@@ -1,5 +1,12 @@
+
 var routesConfig = require('./config/routes');
+
+var overviewCtrl = require("./controller/overviewCtrl");
 
 var app = angular.module('Prog5', ["PointyPony"]);
 
 app.config(routesConfig);
+app.controller('overviewCtrl', overviewCtrl);
+
+//Initialize angular material styling with bootstrap
+$.material.init()
