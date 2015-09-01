b8de50c09877adbacb9ec389ddcf028fe00cfeff
Stijn Smulders <Linksonder@gmail.com>
2015-09-01 10:26:56 +0200

    kleine foutjes (lamda > lambda) gefixt

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,10 +4,10 @@ module.exports = function($stateProvider, $urlRouterProvider) {
     
     $stateProvider
         
-        .state('home', { url: '/home',  templateUrl:'default/home.html' })
+        .state('home', { url: '/home?section',  templateUrl:'default/home.html' })
         .state('profile', { url: '/profile',  templateUrl:'default/profile.html' })
                 
-        .state('week1', { url: '/week1',  templateUrl:'week1/informatie.html', controller: 'overviewCtrl' })
+        .state('week1', { url: '/week1?section',  templateUrl:'week1/informatie.html' })
         .state('week1Opdrachten', { url: '/week1/opdrachten',  templateUrl:'week1/opdrachten.html' })
         
         .state('week2', { url: '/week2',  templateUrl:'week2/informatie.html' })
@@ -19,5 +19,11 @@ module.exports = function($stateProvider, $urlRouterProvider) {
         
         .state('week4', { url: '/week4',  templateUrl:'week4/informatie.html' })
         .state('week4Opdrachten', { url: '/week4/opdrachten',  templateUrl:'week4/opdrachten.html' })
+        
+        .state('week5', { url: '/week5',  templateUrl:'week5/informatie.html' })
+        .state('week5Opdrachten', { url: '/week5/opdrachten',  templateUrl:'week5/opdrachten.html' })
+        
+        .state('week6', { url: '/week6',  templateUrl:'week6/informatie.html' })
+        .state('week6Opdrachten', { url: '/week6opdrachten',  templateUrl:'week6/opdrachten.html' });
    
 };
