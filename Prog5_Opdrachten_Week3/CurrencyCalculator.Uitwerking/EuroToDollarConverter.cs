3231fa713595c16d8b085df6d0e3deca1acae897
Stijn Smulders <Linksonder@gmail.com>
2015-09-22 09:27:19 +0200

    Uitwerkingen week 3

---
+++ Prog5_Opdrachten_Week3/CurrencyCalculator.Uitwerking/EuroToDollarConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CurrencyCalculator
+{
+    public class EuroToDollarConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var result = (double) value * 1.247535d;
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double theValue = double.Parse((string)value);
+
+            var result = theValue / 1.247535d;
+
+            return result;
+            
+        }
+    }
+}
